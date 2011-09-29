c399ad7108b68087d8d262abbbdb17f744bf863b
Alexandre Dumont <adumont@gmail.com>
2011-09-29 11:45:56 +0200

    BOARD_HAS_NO_SELECT_BUTTON

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/htc/marvel/kernel
 
-#BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_NO_SELECT_BUTTON := true
 # Use this flag if the board has a ext4 partition larger than 2gb
 #BOARD_HAS_LARGE_FILESYSTEM := true
 
